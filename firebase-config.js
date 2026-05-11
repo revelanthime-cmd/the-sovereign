a9f029dfd84cb557576a92f4ddd881845186f295
revelanthime-cmd <revelanthime@gmail.com>
2026-05-11 18:28:06 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,14 +3,18 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "TON_PROJECT.firebaseapp.com",
-  projectId: "TON_PROJECT_ID",
-  storageBucket: "TON_PROJECT.appspot.com",
-  messagingSenderId: "TON_SENDER_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyBF1JdikwKg7qVsvJo1QcJDWQga2uHKHEE",
+  authDomain: "habitup-63c2f.firebaseapp.com",
+  projectId: "habitup-63c2f",
+  storageBucket: "habitup-63c2f.firebasestorage.app",
+  messagingSenderId: "179611042542",
+  appId: "1:179611042542:web:7dcc5325f19c2fd331e9ff",
+  measurementId: "G-0KVL2V60CS"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// On exporte les outils dont on a besoin pour le site
 export const db = getFirestore(app);
 export const auth = getAuth(app);
